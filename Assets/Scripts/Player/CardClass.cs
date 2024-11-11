44a2a078bbddba89513c54429f69eb67c46ce6dd
Joshua Baldwin <joshua_baldwin@rudel.jp>
2024-11-11 14:36:52 +0900

    remove unused code; add spacing between players

--- Assets/Scripts/Player/CardClass.cs
+++ Assets/Scripts/Player/CardClass.cs
@@ -10,7 +10,6 @@ namespace THE.Player
 {
     public class CardClass : MonoBehaviour
     {
-        [SerializeField] private Text cardSuit;
         [SerializeField] private Image cardImage;
         [SerializeField] private Text cardRank;
         [SerializeField] private GameObject cover;
@@ -18,15 +17,6 @@ namespace THE.Player
         public void Initialize(CardSuitEnum suit, CardRankEnum rank, bool isOwnCard)
         {
             StartCoroutine(LoadFromResourcesFolder(suit));
-            cardSuit.text = suit switch
-            {
-                CardSuitEnum.Heart => "\u2764\ufe0f",
-                CardSuitEnum.Spade => "\u2660\ufe0f",
-                CardSuitEnum.Diamond => "\u2666\ufe0f",
-                CardSuitEnum.Club => "\u2663\ufe0f",
-                _ => throw new ArgumentOutOfRangeException(nameof(suit), suit, null)
-            };
-            
             cardRank.text = rank.GetDescription();
             cover.SetActive(!isOwnCard);
         }
