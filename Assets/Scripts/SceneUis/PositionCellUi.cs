c9a8391d74272d379301bd0d112c01c7ce13a3e1
Joshua Baldwin <joshua_baldwin@rudel.jp>
2024-12-25 06:28:11 +0900

    position scripts

--- Assets/Scripts/SceneUis/PositionCellUi.cs
+++ Assets/Scripts/SceneUis/PositionCellUi.cs
@@ -1,7 +1,62 @@
+using System;
+using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Linq;
+using THE.Player;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace THE.SceneUis
 {
-    public class PositionCellUi
+    public class PositionCellUi : MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI nameText;
+        [SerializeField] private TextMeshProUGUI roleText;
+        [SerializeField] private TextMeshProUGUI queueOrderText;
+        [SerializeField] private Button selectButton;
+        [SerializeField] private Text selectText;
         
+        private PlayerData playerData;
+        public Action<int> OnSelectAction;
+        public Action<int> OnDeselectAction;
+
+        private bool isSelected;
+
+        private void Awake()
+        {
+            selectButton.OnClickAsAsyncEnumerable()
+                .Subscribe(_ => Select())
+                .AddTo(this.GetCancellationTokenOnDestroy());
+        }
+
+        public void Initialize(PlayerData data)
+        {
+            playerData = data;
+            nameText.text = $"Name: {playerData.Name}";
+            roleText.text = $"Role: {playerData.PlayerRole}";
+            queueOrderText.text = $"Order: {playerData.OrderInQueue}";
+        }
+
+        public void Reset()
+        {
+            isSelected = false;
+            selectText.text = "Select";
+        }
+
+        private void Select()
+        {
+            if (isSelected)
+            {
+                OnDeselectAction?.Invoke(playerData.OrderInQueue);
+                selectText.text = "Select";
+            }
+            else
+            {
+                OnSelectAction?.Invoke(playerData.OrderInQueue);
+                selectText.text = "Deselect";
+            }
+
+            isSelected = !isSelected;
+        }
     }
 }
--- Assets/Scripts/SceneUis/PositionSelectionUi.cs
+++ Assets/Scripts/SceneUis/PositionSelectionUi.cs
@@ -1,7 +1,104 @@
+using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Linq;
+using THE.Player;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace THE.SceneUis
 {
-    public class PsitionSelectionUi
+    public class PositionSelectionUi : MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI newPositionText;
+        [SerializeField] private GameObject positionCellPrefab;
+        [SerializeField] private GameObject contents;
+        [SerializeField] private GameObject positionCellParent;
+        [SerializeField] private Button confirmButton;
+        [SerializeField] private Button closeButton;
         
+        private List<int> newPositions = new();
+        private List<PositionCellUi> positionCells = new();
+
+        private Action<List<int>> onConfirmAction;
+        private Action onCloseAction;
+        private int maxSelection;
+
+        private void Awake()
+        {
+            confirmButton.OnClickAsAsyncEnumerable()
+                .Subscribe(_ => ConfirmSelection())
+                .AddTo(this.GetCancellationTokenOnDestroy());
+            
+            closeButton.OnClickAsAsyncEnumerable()
+                .Subscribe(_ => CancelSelection())
+                .AddTo(this.GetCancellationTokenOnDestroy());
+        }
+
+        public void ShowUi(List<PlayerData> playerList, int maxSelectionCount, Action<List<int>> onConfirm, Action onClose)
+        {
+            newPositionText.text = "New Position: -";
+            maxSelection = maxSelectionCount;
+            onConfirmAction = onConfirm;
+            onCloseAction = onClose;
+            
+            var index = 0;
+            foreach (var playerData in playerList)
+            {
+                var positionCell = Instantiate(positionCellPrefab, positionCellParent.transform).GetComponent<PositionCellUi>();
+                positionCell.Initialize(playerData);
+                positionCell.OnSelectAction = OnSelect;
+                positionCell.OnDeselectAction = OnDeselect;
+                positionCells.Add(positionCell);
+                index++;
+            }
+            contents.SetActive(true);
+            confirmButton.interactable = newPositions.Count == maxSelection;
+        }
+
+        public void Reset()
+        {
+            newPositions.Clear();
+            positionCells.Clear();
+        }
+        
+        private void ConfirmSelection()
+        {
+            onConfirmAction?.Invoke(new List<int>(newPositions));
+            newPositions.Clear();
+            HideUi();
+        }
+
+        private void CancelSelection()
+        {
+            onCloseAction?.Invoke();
+            newPositions.Clear();
+            positionCells.ForEach(position => position.Reset());
+            HideUi();
+        }
+        
+        private void HideUi()
+        {
+            foreach (Transform child in positionCellParent.transform)
+                Destroy(child.gameObject);
+            
+            positionCells.Clear();
+            contents.SetActive(false);
+        }
+
+        private void OnSelect(int position)
+        {
+            newPositionText.text = $"New Position: {position}";
+            newPositions.Add(position);
+            confirmButton.interactable = newPositions.Count == maxSelection;
+        }
+        
+        private void OnDeselect(int position)
+        {
+            newPositionText.text = $"New Position: -";
+            newPositions.Remove(position);
+            confirmButton.interactable = newPositions.Count == maxSelection;
+        }
     }
 }
